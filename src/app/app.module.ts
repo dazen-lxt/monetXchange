605e4da908739609f4264c7ad1350935bb936306
kernel688 <jcmartinezcano@gmail.com>
2022-04-22 12:27:15 -0500

    Material Design Problem Solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     ReactiveFormsModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatInputModule
 
   ],
   providers: [
